dbf081c1d9b5d29fe225a13c70c123ce3a2db79f
ALberto Garcia <albertogarcia2220@gmail.com>
2024-02-22 16:37:44 -0700

    Changed Navbar and carousel

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
 import Logo from "../assets/Logo.jpg";
-import "../styles/Navbar.css";
-import ReorderIcon from "@mui/icons-material/Reorder";
+import "../styles/Navbar.css"; // Import custom CSS file for Navbar styles
 
-function Navbar() {
-  // State to control the expansion of the navbar
-  const [expandNavbar, setExpandNavbar] = useState(false);
-
-  // Access the current location using the useLocation hook from react-router-dom
-  const location = useLocation();
-
-  // Reset the expansion of the navbar whenever the location changes
-  useEffect(() => {
-    setExpandNavbar(false);
-  }, [location]);
+function CustomNavbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="navbar" id={expandNavbar ? "open" : "close"}>
-      <div className="toggleButton">
-        {/* Button to toggle the expansion of the navbar */}
-        <button onClick={() => setExpandNavbar((prev) => !prev)}>
-          <ReorderIcon />
-        </button>
-      </div>
-      <div className="links">
-        {/* Links to different routes */}
-        <img src={Logo} className="logo" />
-        <Link to="/Just-Cake">Home</Link>
-        <Link to="/Just-Cake/cakes">Cakes</Link>
-        <Link to="/Just-Cake/weddingCakes">Wedding Cakes</Link>
-        <Link to="/Just-Cake/cupcakes">Cupcakes</Link>
-        <Link to="/Just-Cake/cookies">Cookies</Link>
-      </div>
+    <div className="navbar-container"> {/* Wrapping Navbar in a container */}
+      <Navbar expand="md" className="custom-navbar"> {/* Adding custom class to Navbar */}
+        <NavbarBrand className="ms-5" href="/just-cake">
+          <img src={Logo} alt="logo" className="logo" />
+        </NavbarBrand>
+        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+        <Collapse isOpen={menuOpen} navbar>
+          <Nav className="ms-auto" navbar>
+            <NavItem>
+              <Link className="nav-link" to="Just-Cake/">
+                <i className="fa fa-home fa-lg" /> Home
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="Just-Cake/cakes">
+                <i className="fa fa-list fa-lg" /> Cakes
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="Just-Cake/cookies">
+                <i className="fa fa-info fa-lg" /> Cookies
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link className="nav-link" to="Just-Cake/weddingCakes">
+                <i className="fa fa-address-card fa-lg" /> Wedding Cakes
+              </Link>
+            </NavItem>
+              <Link className="nav-link" to="Just-Cake/cupcakes">
+                <i className="fa fa-address-card fa-lg" /> Cupcakes
+              </Link>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </div>
   );
 }
 
-export default Navbar;
+export default CustomNavbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,74 @@
-import React from "react";
 import "../styles/Home.css";
-import { Carousel } from "react-bootstrap";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
-import cake1 from "../assets/cake1.jpeg";
-import cake2 from "../assets/cake2.jpeg";
-import cake3 from "../assets/cake3.jpeg";
+import {
+  Container,
+  Row,
+  Col,
+  Carousel,
+} from "react-bootstrap";
+// import cake1 from "../assets/cake1.jpeg";
+// import cake2 from "../assets/cake2.jpeg";
+// import cake3 from "../assets/cake3.jpeg";
 
 function Home() {
-  const imageStyle = {
-    width: "250px",
-    borderRadius: "50%",
-  };
+  // const imageStyle = {
+  //   width: "250px",
+  //   borderRadius: "50%",
+  // };
+
+  // Define items array outside of the return statement
+  const items = [
+    {
+      src: 'https://picsum.photos/id/123/1200/400',
+      altText: 'Slide 1',
+      caption: 'Slide 1',
+      key: 1,
+    },
+    {
+      src: 'https://picsum.photos/id/456/1200/400',
+      altText: 'Slide 2',
+      caption: 'Slide 2',
+      key: 2,
+    },
+    {
+      src: 'https://picsum.photos/id/678/1200/400',
+      altText: 'Slide 3',
+      caption: 'Slide 3',
+      key: 3,
+    },
+  ];
 
   return (
-    <div className="home">
-      <div className="about">
-        <h2>Rosie Bake Me A Cake</h2>
-        <div className="prompt">
-          <p>
-            A software developer with a passion for learning and creating
-            beautiful websites.
-          </p>
-        </div>
-      </div>
-      <div className="carousel-container">
-        <Carousel
-          className="carousel"
-          prevIcon={<BsChevronLeft />}
-          nextIcon={<BsChevronRight />}
-        >
-          <Carousel.Item className="carousel-item" interval={5000}>
-            <img src={cake1} className="carousel-img" alt="First slide" />
+    <Container fluid>
+      <Row>
+        <Col>
+          <div className="home">
+            <div className="about">
+              <h2>Rosie Bake Me A Cake</h2>
+              <div className="prompt">
+                <p>
+                  A software developer with a passion for learning and creating
+                  beautiful websites.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Carousel>
+        {items.map(item => (
+          <Carousel.Item key={item.key}>
+            <img
+              className="d-block w-100"
+              src={item.src}
+              alt={item.altText}
+            />
             <Carousel.Caption>
-              <h3 className="carousel-label">First slide label</h3>
-              <p className="carousel-text">
-                Nulla vitae elit libero, a pharetra augue mollis interdum.
-              </p>
+              <h3>{item.caption}</h3>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={5000}>
-            <img src={cake2} className="carousel-img" alt="Second slide" />
-            <Carousel.Caption>
-              <h3 className="carousel-label">Second slide label</h3>
-              <p className="carousel-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={5000}>
-            <img src={cake3} className="carousel-img" alt="Third slide" />
-            <Carousel.Caption>
-              <h3 className="carousel-label">Third slide label</h3>
-              <p className="carousel-text">
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-      </div>
-    </div>
+        ))}
+      </Carousel>
+    </Container>
   );
 }
 
