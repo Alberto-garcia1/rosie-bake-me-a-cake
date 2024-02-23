34059afce4030373ce058ea2dedf67a2ce86418c
ALberto Garcia <albertogarcia2220@gmail.com>
2024-02-22 17:00:29 -0700

    fixed toggle menu issue css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,55 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Collapse,
+  NavbarToggler,
+  Nav,
+  NavItem,
+} from "reactstrap";
 import Logo from "../assets/Logo.jpg";
-import "../styles/Navbar.css"; // Import custom CSS file for Navbar styles
+import "../styles/Navbar.css";
 
 function CustomNavbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <div className="navbar-container"> {/* Wrapping Navbar in a container */}
-      <Navbar expand="md" className="custom-navbar"> {/* Adding custom class to Navbar */}
+    <div className="navbar-container">
+      <Navbar expand="md" className="custom-navbar">
         <NavbarBrand className="ms-5" href="/just-cake">
           <img src={Logo} alt="logo" className="logo" />
         </NavbarBrand>
-        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+        <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} className="menu"/>
         <Collapse isOpen={menuOpen} navbar>
           <Nav className="ms-auto" navbar>
             <NavItem>
               <Link className="nav-link" to="Just-Cake/">
-                <i className="fa fa-home fa-lg" /> Home
+                Home
               </Link>
             </NavItem>
             <NavItem>
               <Link className="nav-link" to="Just-Cake/cakes">
-                <i className="fa fa-list fa-lg" /> Cakes
+                Cakes
               </Link>
             </NavItem>
             <NavItem>
               <Link className="nav-link" to="Just-Cake/cookies">
-                <i className="fa fa-info fa-lg" /> Cookies
+                Cookies
               </Link>
             </NavItem>
             <NavItem>
               <Link className="nav-link" to="Just-Cake/weddingCakes">
-                <i className="fa fa-address-card fa-lg" /> Wedding Cakes
+                Wedding Cakes
               </Link>
             </NavItem>
+            <NavItem>
+              {" "}
+              {/* Added wrapping NavItem */}
               <Link className="nav-link" to="Just-Cake/cupcakes">
                 <i className="fa fa-address-card fa-lg" /> Cupcakes
               </Link>
+            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
